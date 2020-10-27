435c6f2934e940a67fbbb16e9054cc497b27ebc9
Alex Olszewski <alexander.olszewski@gmail.com>
2020-10-27 19:58:03 -0400

    added server package.json

--- client/src/components/Welcome/index.js
+++ client/src/components/Welcome/index.js
@@ -8,7 +8,7 @@ import './style.css';
 
 function Welcome(props) 
 {
-    const [User, setUser] = useState({})
+    const [user, setUser] = useState({})
 
     const { id } = useParams();
     useEffect(() => {
@@ -20,11 +20,10 @@ function Welcome(props)
         .catch(err => console.log(err));
     }, [])
 
-
     return(
         <div class='col-12'>
             <section class='welcome'>
-                {/* <h1>Hello <p>{user.username}</p></h1> */}
+                <h1>Hello <p>{user.username}</p></h1>
                 <h1>Hello</h1>
                 <p class='AofW-text'>Meet and connect with your follow readers</p>
             </section>
